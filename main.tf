e96edb9855d13efa309e5e424ac5f7b59e377c5a
slama34849 <suryarajlama@gmail.com>
2021-03-02 12:07:27 -0500

    removing provider block

--- main.tf
+++ main.tf
@@ -10,12 +10,10 @@
 #   }
 # }
 #Provider Configuration
-provider "aws" {
-    region = "us-east-1"
-    access_key = var.access_key
-    secret_key = var.secret_key
-}
 #S3 Bucket Configuration
 resource "aws_s3_bucket" "test" {
     bucket_prefix = "test-"
 }
+
+
+
